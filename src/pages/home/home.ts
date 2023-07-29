f72c9504dfc8595ef32de18b2157abb0b934c045
Laura Bigoni <laurabigoni.92@gmail.com>
2023-07-29 18:47:19 +0200

    linked stylesheet to pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.html',
+  styleUrls: ['./home.scss'],
 })
 export class HomeComponent {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/pages/not-found/not-found.ts
+++ src/pages/not-found/not-found.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'not-found',
   templateUrl: './not-found.html',
+  styleUrls: ['./not-found.scss'],
 })
 export class NotFoundComponent {
   constructor() {}
