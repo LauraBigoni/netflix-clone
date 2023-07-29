51296b1fc489c114c8b5347227230373b10dbf79
Laura Bigoni <laurabigoni.92@gmail.com>
2023-07-29 20:23:14 +0200

    add header / add login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from 'src/pages/home/home';
 import { LoginComponent } from 'src/pages/login/login';
 import { NotFoundComponent } from 'src/pages/not-found/not-found';
+import { HeaderComponent } from 'src/components/header/header';
+import { FooterComponent } from 'src/components/footer/footer';
+
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     LoginComponent,
     HomeComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
